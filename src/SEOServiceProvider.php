2a7cf85614dd5ddece2d84d7cc1ceb2b5cf3c6b2
adonishhh772 <nightguyme@gmail.com>
2023-08-11 21:29:41 +0100

    ALL OPTIONS

---
+++ src/SEOServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ArchTech\SEO;
+
+use ArchTech\SEO\Commands\GenerateFaviconsCommand;
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\ServiceProvider;
+
+class SEOServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        $this->app->singleton('seo', SEOManager::class);
+    }
+
+    public function boot(): void
+    {
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'seo');
+
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                GenerateFaviconsCommand::class,
+            ]);
+        }
+
+        $this->publishes([
+            __DIR__ . '/../resources/views' => resource_path('views/vendor/seo'),
+        ], 'seo-views');
+
+        Blade::directive('seo', function ($expression) {
+            return "<?php echo seo()->render($expression); ?>";
+        });
+    }
+}
